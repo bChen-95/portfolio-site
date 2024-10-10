caac59e6cec874204d3ab43aa8ea2b5f977b985f
Brice Chen <brice_chen@hotmail.com>
2024-10-10 15:45:25 -0700

    2023 to 2024

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
    
 return(
 <footer className="mt-8 pb-16 lg:pb-4">
- <p className='text-center' >&#169; 2023 | Brice Chen </p>
+ <p className='text-center' >&#169; 2024 | Brice Chen </p>
 </footer>
 
 )
